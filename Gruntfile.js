e021f044e6666a7014a0c318916ef69e78d6f0fb
Nick Gaswirth <nick@roundhouse-designs.com>
2015-01-08 17:42:32 -0500

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint : {
+      all : ['Gruntfile.js', 'js/*.js', '!js/**/*modernizr*.js', '!js/**/*.min.js', '!js/vendor/**/*.js']
+    },
+    watch : {
+      js : {
+        files: ['js/**/*.js'],
+        tasks : ['jshint'],
+        options : {
+          livereload : true
+        }
+      },
+      php : {
+        files : ['**/*.php'],
+        options : {
+          livereload : true
+        }
+      },
+      css : {
+        files : ['**/*.css'],
+        options : {
+          livereload : true
+        }
+      }
+    },
+    modernizr : {
+      dist : {
+        'devFile' : 'js/vendor/modernizr/modernizr-dev.js',
+        'outputFile' : 'js/vendor/modernizr/modernizr-custom.js'
+      }
+    },
+    clean : {
+      files : ['../twenty*', '.gitignore'],
+      options : {
+        force : true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('default', ['jshint']);
+  
+  grunt.registerTask('setup', ['clean']);
+};
