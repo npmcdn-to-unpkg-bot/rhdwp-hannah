47b1b172aee72aaf1212174d8ba8011defae773a
Nick Gaswirth <nick@roundhouse-deisgns.com>
2015-01-21 08:07:16 -0800

    Quick fix

--- page.php
+++ page.php
@@ -17,6 +17,8 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 			<?php endwhile; ?>
+			
+		<?php endif; ?>
 
 		</div><!-- #content -->
 		
