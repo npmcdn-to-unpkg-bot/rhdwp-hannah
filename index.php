2575135482e8df46228f833d30280d6eaf1bce3f
Nick Gaswirth <nick@roundhouse-designs.com>
2015-02-13 14:51:56 -0500

    Tweaks

--- index.php
+++ index.php
@@ -11,46 +11,46 @@ get_header(); ?>
 	<section id="primary" class="site-content">
 		<div id="content" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content' ); ?>
-			<?php endwhile; ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content' ); ?>
+				<?php endwhile; ?>
 
-			<?php if ( is_single() && comments_open() ) comments_template(); ?>
+				<?php if ( is_single() && comments_open() ) comments_template(); ?>
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<article id="post-0" class="post no-results not-found">
+				<article id="post-0" class="post no-results not-found">
 
-			<?php if ( current_user_can( 'edit_posts' ) ) :
-				// Show a different message to a logged-in user who can add posts.
-			?>
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'No posts to display', 'rhd' ); ?></h1>
-				</header>
+				<?php if ( current_user_can( 'edit_posts' ) ) :
+					// Show a different message to a logged-in user who can add posts.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'No posts to display', 'rhd' ); ?></h1>
+					</header>
 
-				<div class="entry-content">
-					<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'rhd' ), admin_url( 'post-new.php' ) ); ?></p>
-				</div><!-- .entry-content -->
+					<div class="entry-content">
+						<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'rhd' ), admin_url( 'post-new.php' ) ); ?></p>
+					</div><!-- .entry-content -->
 
-			<?php else :
-				// Show the default message to everyone else.
-			?>
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Nothing Found', 'rhd' ); ?></h1>
-				</header>
+				<?php else :
+					// Show the default message to everyone else.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'rhd' ); ?></h1>
+					</header>
 
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'rhd' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			<?php endif; // end current_user_can() check ?>
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'rhd' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				<?php endif; // end current_user_can() check ?>
 
-			</article><!-- #post-0 -->
+				</article><!-- #post-0 -->
 
-		<?php endif; // end have_posts() check ?>
+			<?php endif; // end have_posts() check ?>
 
 		</div><!-- #content -->
 
