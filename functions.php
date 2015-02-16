52f3814d37711d5d122ab39ed8ac29bae79f027d
Nick Gaswirth <nick@roundhouse-designs.com>
2015-02-16 11:27:58 -0500

    Commented out Slidebars JS/CSS lines by default

--- functions.php
+++ functions.php
@@ -31,7 +31,8 @@ define( 'DISALLOW_FILE_EDIT', true );
 function rhd_enqueue_styles(){
 	wp_enqueue_style( 'rhd-main', RHD_THEME_DIR . '/css/main.css', array(), '1', 'all' );
 	wp_enqueue_style( 'normalize', RHD_THEME_DIR . '/css/normalize.css', array( 'slidebars-js-css' ), null, 'all' );
-	wp_enqueue_style( 'slidebars-js-css', RHD_THEME_DIR . '/js/vendor/Slidebars/distribution/0.10.2/slidebars.min.css', null, '0.10.0' );
+
+	// wp_enqueue_style( 'slidebars-js-css', RHD_THEME_DIR . '/js/vendor/Slidebars/distribution/0.10.2/slidebars.min.css', null, '0.10.0' );
 
 	// Web Fonts
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:400,400italic,700|Rock+Salt' );
@@ -43,10 +44,11 @@ function rhd_enqueue_styles(){
 add_action( 'wp_enqueue_scripts', 'rhd_enqueue_styles' );
 
 function rhd_enqueue_scripts() {
-	wp_enqueue_script( 'rhd-plugins', RHD_THEME_DIR . '/js/plugins.js', array( 'jquery' ), null, true );
-	wp_enqueue_script( 'rhd-main', RHD_THEME_DIR . '/js/main.js', array( 'rhd-plugins', 'jquery', 'slidebars-js' ), null, true );
 	wp_enqueue_script( 'modernizr', RHD_THEME_DIR . '/js/vendor/modernizr/modernizr-custom.js', null, '2.8.3', true );
-	wp_enqueue_script( 'slidebars-js', RHD_THEME_DIR . '/js/vendor/Slidebars/distribution/0.10.2/slidebars.min.js', null, '0.10.2', true );
+	wp_enqueue_script( 'rhd-plugins', RHD_THEME_DIR . '/js/plugins.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'rhd-main', RHD_THEME_DIR . '/js/main.js', array( 'rhd-plugins', 'jquery'/* , 'slidebars-js' */ ), null, true );
+
+	// wp_enqueue_script( 'slidebars-js', RHD_THEME_DIR . '/js/vendor/Slidebars/distribution/0.10.2/slidebars.min.js', null, '0.10.2', true );
 
 	if ( is_singular() )
 		wp_enqueue_script( 'comment-reply' );
