437a078b0024992396493a8505d2c130de315583
Nick Gaswirth <gaswirth@gmail.com>
2015-02-26 14:59:42 -0500

    fixed text domain

--- 404.php
+++ 404.php
@@ -13,11 +13,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'rhd' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'rhd' ); ?></p>
 
 					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
