117d7205ce6c52ec71e0b6f86bdffa00f307b82e
Nick Gaswirth <gaswirth@gmail.com>
2015-02-26 14:11:17 -0500

    Added basic 404

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
+
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+
+		</div><!-- #content -->
+
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
