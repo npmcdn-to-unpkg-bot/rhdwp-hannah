67f5404d92491e30b5a4f7d8af2f44032413d7ef
Nick Gaswirth <gaswirth@joanna.roundhouse-designs.com>
2015-04-26 11:12:06 -0400

    Removed silly YouTube Responsivizer function that never really worked

--- js/main.js
+++ js/main.js
@@ -49,8 +49,6 @@ function rhdInit() {
 	if ( wp_data.inc_packery === true ) {
 		// Packery Initialization + Options
 	}
-
-	rhd_youtube_responsivizer();
 }
 
 
@@ -58,39 +56,4 @@ function wpadminbarPush() {
 	jQuery("#wpadminbar").css({
 		top: '50px',
 	});
-}
-
-
-function rhd_youtube_responsivizer() {
-	var $allVideos = jQuery("iframe[src^='//www.youtube.com']"),
-
-	// The element that is fluid width
-	$fluidEl = jQuery(".entry-content");
-
-	// Figure out and save aspect ratio for each video
-	$allVideos.each(function() {
-
-	jQuery(this)
-		.data('aspectRatio', this.height / this.width)
-
-		// and remove the hard coded width/height
-		.removeAttr('height')
-		.removeAttr('width');
-	});
-
-	// When the window is resized
-	jQuery(window).resize(function() {
-
-		var newWidth = $fluidEl.width();
-
-		// Resize all videos according to their own aspect ratio
-		$allVideos.each(function() {
-			var $el = jQuery(this);
-			$el
-			.width(newWidth)
-			.height(newWidth * $el.data('aspectRatio'));
-		});
-
-	// Kick off one resize to fix all videos on page load
-	}).resize();
 }
