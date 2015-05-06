635cb52b9962b54c2d5e9e09cc34413ad0fac710
Nick Gaswirth <nick@roundhouse-designs.com>
2015-05-06 11:17:23 -0400

    Changed h1 to h2 in content*.php files

--- content.php
+++ content.php
@@ -10,11 +10,11 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<?php if ( is_single() ) : ?>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<h2 class="entry-title"><?php the_title(); ?></h2>
 			<?php else : ?>
-				<h1 class="entry-title">
+				<h2 class="entry-title">
 					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h1>
+				</h2>
 			<?php endif; // is_single() ?>
 		</header><!-- .entry-header -->
 
