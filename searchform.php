6aa7f93892167b392c1d96922a6256a1ccc4fbb0
Nick Gaswirth <gaswirth@gmail.com>
2015-05-13 13:50:55 -0400

    Added search-field class to searchform input

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>">
     <div>
-        <input type="text" value="" placeholder="I'm looking for..." name="search" />
+        <input type="text" value="" class="search-field" placeholder="I'm looking for..." name="search" />
         <input type="submit" class="search-submit" value="Search" />
     </div>
 </form>
