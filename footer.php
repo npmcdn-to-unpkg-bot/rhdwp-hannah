3876aa3f30f2371dc1a1d273d72e782617053d8f
Nick Gaswirth <gaswirth@gmail.com>
2015-07-22 13:31:59 -0400

    HTML5 semantics tweaks

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	</main><!-- #main -->
 
-    <footer id="colophon" role="contentinfo">
+    <footer id="colophon">
         <?php get_sidebar( 'footer' ); ?>
         <div class="site-info">
 			<p>
--- header.php
+++ header.php
@@ -73,7 +73,7 @@
 		</div>
 
 		<div id="page" class="hfeed site sb-site-container">
-			<header id="masthead" class="site-header" role="banner">
+			<header id="masthead" class="site-header">
 				<h1 id="site-title" class="site-title"><?php bloginfo( 'name' ); ?></h1>
 
 				<?php wp_nav_menu( $nav_args_main ); ?>
