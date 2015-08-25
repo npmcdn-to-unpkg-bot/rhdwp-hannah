ba38e3ea7777be3cd4578ac2b5dbe45e11e04731
Nick Gaswirth <gaswirth@gmail.com>
2015-08-25 14:15:42 -0400

    Tweaks

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -10,7 +10,7 @@
 ?>
 
 <?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
-	<aside id="footer-widget-area" class="widget-area" role="complementary">
+	<aside id="footer-widget-area" class="widget-area">
 		<?php dynamic_sidebar( 'footer-widget-area' ); ?>
 	</aside><!-- #secondary -->
 <?php endif; ?>
