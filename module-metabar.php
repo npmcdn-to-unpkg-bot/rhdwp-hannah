ca3cf68a60e31115992a31401d5c2baf3a0ab1bb
Nick Gaswirth <gaswirth@gmail.com>
2015-10-01 11:37:56 -0400

    Added media query desktop styles to _metbar.styl partial

--- module-metabar.php
+++ module-metabar.php
@@ -8,8 +8,8 @@
  */
 ?>
 
-<div id="blog-meta-area">
-	<ul id="blog-meta-content">
+<div id="blog-metabar">
+	<ul id="blog-metabar-content">
 		<li>
 			<div id="blog-categories" class="rhd-dropdown">
 				<div class="rhd-dropdown-title">
