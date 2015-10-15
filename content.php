d7ef236875040ed6adeeb91c1949d8e02dd957f6
Nick Gaswirth <nick@roundhouse-designs.com>
2015-10-15 10:25:12 -0400

    content.php no longer has single post checks

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The default template for displaying content. Used for both single and index/archive/search, as well as home page.
+ * The default template for displaying content. Used for both index/archive/search.
  *
  * @package WordPress
  * @subpackage rhd
@@ -9,13 +9,9 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<?php if ( is_single() ) : ?>
-				<h2 class="entry-title"><?php the_title(); ?></h2>
-			<?php else : ?>
-				<h2 class="entry-title">
-					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h2>
-			<?php endif; // is_single() ?>
+			<h2 class="entry-title">
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+			</h2>
 			<p class="entry-details">By <?php the_author(); ?> <span class="sep">|</span> <?php  the_time( get_option( 'date_format' ) ); ?></p>
 		</header><!-- .entry-header -->
 
