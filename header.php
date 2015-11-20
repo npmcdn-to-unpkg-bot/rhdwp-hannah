bfb808631626f1ee1875138f725559cd0fd23c3b
Nick Gaswirth <nick@roundhouse-designs.com>
2015-11-20 12:05:14 -0500

    validation tweaks

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta http-equiv="X-UA-Compatible" content="IE=9" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 		<!--
