10a09a298bec0dd86a576d01c3aa0a1ec8c0f85b
Nick Gaswirth <nick@roundhouse-designs.com>
2016-03-16 16:28:24 -0400

    building

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ get_header();
 ?>
 
 	<section id="primary" class="site-content">
-		<?php if ( $_SESSION['blog_area'] === true ) get_template_part( 'module', 'metabar' ); ?>
-
 		<div id="content" role="main">
 
 			<?php if ( have_posts() ) : ?>
--- page.php
+++ page.php
@@ -25,5 +25,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
