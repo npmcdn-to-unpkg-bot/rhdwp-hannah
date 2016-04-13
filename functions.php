a5b0525006ee8329bcf384f0821ef43a52cdf170
Nick Gaswirth <nick@roundhouse-designs.com>
2016-04-13 17:23:58 -0400

    Fixed blog-area #primary bug

--- functions.php
+++ functions.php
@@ -621,7 +621,7 @@ function rhd_body_class( $body_classes )
 	$body_classes[] = ( !wp_is_mobile() && !rhd_is_mobile() ) ? 'desktop' : '';
 
 	//session_start();
-	if ( is_home() || is_single() || is_archive() || is_search() ) {
+	if ( is_home() || is_archive() || is_search() ) {
 		$body_classes[] = 'blog-area';
 
 		$_SESSION['blog_area'] = true;
