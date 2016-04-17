1bc4e3e106c3d9ae6aaec14ba77e0257484dd5d1
Nick Gaswirth <nick@roundhouse-designs.com>
2016-04-17 11:48:14 -0400

    Added project rich text area

---
+++ sidebar-project.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * If no active widgets in sidebar, let's hide it completely.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+<?php if ( is_active_sidebar( 'sidebar-project' ) ) : ?>
+	<section id="secondary" class="single-project-sidebar widget-area">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="widget widget-featured-image">
+				<?php the_post_thumbnail( 'medium' ); ?>
+			</div>
+		<?php endif; ?>
+		
+		<?php dynamic_sidebar( 'sidebar-project' ); ?>
+	</section><!-- #sidebar-project -->
+<?php endif; ?>
