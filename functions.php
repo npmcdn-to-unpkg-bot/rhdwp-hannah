76bab5303b8c2a2cac64aecca769f16f240dbd4b
Nick Gaswirth <nick@roundhouse-designs.com>
2016-04-20 22:03:07 -0400

    Added ghost button shortcode

--- functions.php
+++ functions.php
@@ -665,3 +665,33 @@ function rhd_svg_logo()
 /* ==========================================================================
 	Theme Functions and Customizations
    ========================================================================== */
+
+/**
+ * rhd_ghost_button_shortcode function.
+ *
+ * @access public
+ * @param mixed $atts
+ * @param mixed $content (default: null)
+ * @return void
+ */
+function rhd_ghost_button_shortcode( $atts, $content = null )
+{
+        $a = shortcode_atts( array(
+                'url' => '',
+                'target' => ''
+        ), $atts );
+
+        extract($a);
+
+        if ( $target )
+                $target = "target={$target}";
+        else
+                $target = '';
+
+        $output = "
+                <div class='ghost-button'><a href='{$url}' {$target}>{$content}</a></div>
+        ";
+
+        return $output;
+}
+add_shortcode( 'ghost-button', 'rhd_ghost_button_shortcode' );
