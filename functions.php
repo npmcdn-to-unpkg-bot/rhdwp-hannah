815240678d7db546bf1ef811f2f66be6aa3c3be8
Nick Gaswirth <gaswirth@gmail.com>
2016-05-03 14:11:31 -0400

    Moved rhd_body_class() to rhd-theme.php

--- functions.php
+++ functions.php
@@ -579,33 +579,3 @@ function rhd_title_check_hidden( $title )
 	return $title;
 }
 add_filter( 'widget_title', 'rhd_title_check_hidden' );
-
-
-/**
- * rhd_body_class function.
- *
- * @access public
- * @return void
- */
-function rhd_body_class( $body_classes )
-{
-	// Basic front page & device detection
-	$body_classes[] = ( is_front_page() ) ? 'front-page' : '';
-	$body_classes[] = ( rhd_is_mobile() ) ?  'mobile' : '';
-	$body_classes[] = ( wp_is_mobile() && ! rhd_is_mobile() ) ? 'tablet' : '';
-	$body_classes[] = ( ! wp_is_mobile() && ! rhd_is_mobile() ) ? 'desktop' : '';
-
-	session_start();
-
-	if ( is_home() || is_single() || is_archive() || is_search() ) {
-		$body_classes[] = 'blog-area';
-
-		$_SESSION['blog_area'] = true;
-	} else {
-		$_SESSION['blog_area'] = false;
-	}
-	session_write_close();
-
-	return $body_classes;
-}
-add_filter( 'body_class', 'rhd_body_class' );
--- inc/rhd-theme.php
+++ inc/rhd-theme.php
@@ -9,3 +9,33 @@
  * @package WordPress
  * @subpackage rhdwp-hannah
  **/
+
+
+ /**
+ * rhd_body_class function.
+ *
+ * @access public
+ * @return void
+ */
+function rhd_body_class( $body_classes )
+{
+	// Basic front page & device detection
+	$body_classes[] = ( is_front_page() ) ? 'front-page' : '';
+	$body_classes[] = ( rhd_is_mobile() ) ?  'mobile' : '';
+	$body_classes[] = ( wp_is_mobile() && ! rhd_is_mobile() ) ? 'tablet' : '';
+	$body_classes[] = ( ! wp_is_mobile() && ! rhd_is_mobile() ) ? 'desktop' : '';
+
+	session_start();
+
+	if ( is_home() || is_single() || is_archive() || is_search() ) {
+		$body_classes[] = 'blog-area';
+
+		$_SESSION['blog_area'] = true;
+	} else {
+		$_SESSION['blog_area'] = false;
+	}
+	session_write_close();
+
+	return $body_classes;
+}
+add_filter( 'body_class', 'rhd_body_class' );
