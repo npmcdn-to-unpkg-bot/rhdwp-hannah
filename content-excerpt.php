40944d793804aa8117f44d3ba74b2fd4678f4ba4
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-03 14:29:37 -0400

    Basic blog feed query set up

---
+++ content-excerpt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The default template for displaying content. Used for both index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h2 class="entry-title">
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+			</h2>
+			<p class="entry-details"><?php  the_time( get_option( 'date_format' ) ); ?><span class="sep"> | </span><span class="entry-cats"><?php the_category( ', ' ); ?></span></p>
+		</header><!-- .entry-header -->
+
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+
+		<footer class="entry-meta">
+			<p><?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post -->
---
+++ page-blog.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+<section id="primary" class="site-content blog-area">
+	<div id="content" role="main">
+		<?php
+		$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+		$i = 0; // Post counter
+
+		$args = array(
+			'post__in' => get_option( 'sticky_posts' ),
+			'ignore_sticky_posts' => 1,
+			'posts_per_page' => get_option( 'posts_per_page' ),
+			'paged' => $paged
+		);
+		$q = new WP_Query( $args );
+		?>
+
+		<?php if ( $q->have_posts() ) : ?>
+
+			<?php while ( $q->have_posts() ) : $q->the_post(); ?>
+				<?php
+				++$i;
+				if ( $i == 1 && $paged == 1 )
+					get_template_part( 'content', 'full' );
+				else
+					get_template_part( 'content', 'excerpt' );
+				?>
+			<?php endwhile; ?>
+
+			<?php rhd_archive_pagination( $q ); ?>
+			<?php wp_reset_postdata(); ?>
+
+		<?php endif; ?>
+
+	</div><!-- #content -->
+</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
