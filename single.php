b57eeab1b34ee5db7b968f035970e1c63a92419d
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-13 23:26:50 -0400

    Comments finished

---
+++ single.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+						if ( is_single() ) get_template_part( 'content', 'single' );
+						else get_template_part( 'content', 'full' );
+					?>
+				<?php endwhile; ?>
+
+				<?php if ( comments_open() ) comments_template(); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+
+				<?php if ( current_user_can( 'edit_posts' ) ) :
+					// Show a different message to a logged-in user who can add posts.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'No posts to display', 'rhd' ); ?></h1>
+					</header>
+
+					<div class="entry-content">
+						<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'rhd' ), admin_url( 'post-new.php' ) ); ?></p>
+					</div>
+
+				<?php else :
+					// Show the default message to everyone else.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'rhd' ); ?></h1>
+					</header>
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'rhd' ); ?></p>
+						<?php get_search_form(); ?>
+					</div>
+				<?php endif; // end current_user_can() check ?>
+
+				</article>
+
+			<?php endif; // end have_posts() check ?>
+
+			<?php rhd_single_pagination(); ?>
+		</div>
+
+	</section>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
