93a7c3b60403b1b2743b8e6d988d5fbbfbaa30dd
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-25 10:49:46 -0400

    front page unstyled

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+<section id="primary" class="site-content full-width">
+	<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+	</div><!-- #content -->
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
