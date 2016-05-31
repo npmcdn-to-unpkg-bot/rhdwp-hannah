035c5e296b85e0eb61d0b2e093521265cbe8d30b
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-31 12:46:33 -0400

    updates done

--- front-page.php
+++ front-page.php
@@ -14,6 +14,26 @@ get_header(); ?>
 			<div class="home-big-img">
 				<img alt="Matt Greenberg" src="<?php echo RHD_IMG_DIR; ?>/home-retouched.jpg" alt="Matt Greenberg">
 			</div>
+			
+			<?php
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'posts_per_page' => 3
+			);
+			$news_q = new WP_Query( $args );
+			?>
+
+			<?php if ( $news_q->have_posts() ) : ?>
+				<div class="news-grid">
+					<?php while ( $news_q->have_posts() ) : $news_q->the_post(); ?>
+	
+						<?php get_template_part( 'content', 'grid' ); ?>
+	
+					<?php endwhile; ?>
+				</div>
+				<div class="ghost-button morenews"><a href="<?php echo home_url( '/news' ); ?>">More News</a></div>
+			<?php endif; ?>
 
 		</div><!-- #content -->
 	</section><!-- #primary -->
