1320e23b015670493972f995aee11effc6187a46
Nick Gaswirth <nick@roundhouse-designs.com>
2016-06-02 00:24:56 -0400

    front page basic scaffold (unstyled)

--- front-page.php
+++ front-page.php
@@ -11,15 +11,43 @@ get_header(); ?>
 <section id="primary" class="site-content full-width">
 	<div id="content" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<section id="front-page-features">
+			
+		</section>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; ?>
-
-		<?php endif; ?>
+		<section id="front-page-intro">
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</section>
+		
+		<section id="front-page-sections">
+			
+		</section>
+		
+		<section id="front-page-video">
+			
+		</section>
+		
+		<section id="front-page-posts">
+			<?php
+			$args = array(
+				'post_type' => 'post',
+				'posts_per_page' => 3,
+				'post_status' => 'publish'
+			);
+			
+			$fpq = new WP_Query( $args );
+			?>
+			
+			<?php if ( $fpq->have_posts() ) : ?>
+				<?php while ( $fpq->have_posts() ) : $fpq->the_post(); ?>
+					<?php get_template_part( 'template-parts/content', 'recent' ); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</section>
 
 	</div><!-- #content -->
 </section><!-- #primary -->
