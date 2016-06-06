ea88f4d447a96c22f95afacfa6f1732231dbaaa0
Nick Gaswirth <nick@roundhouse-designs.com>
2016-06-06 01:27:07 -0400

    dev

---
+++ template-parts/content-recent.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The default template for displaying content. Used for both index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'recent-post' ); ?>>
+		<header class="entry-header">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+				<div class="recent-post-thumbnail">
+					<?php the_post_thumbnail( 'square' ); ?>
+				</div>
+				<h2 class="entry-title">
+					<?php the_title(); ?>
+				</h2>
+			</a>
+		</header><!-- .entry-header -->
+
+		<footer class="entry-meta">
+			<p><?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post -->
