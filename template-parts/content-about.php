1914422368fe6728023800d32f86c4f47690edc2
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-05 21:07:57 -0400

    About text updated

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -7,25 +7,28 @@
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h2 class="page-title ff-courier"><?php the_title(); ?></h2>
-		</header><!-- .entry-header -->
+<?php $quote = get_post_custom_values( 'about_quote' ); ?>
 
-		<div class="entry-content">
-			<?php
-			if ( has_post_thumbnaiL() ) {
-				$thumb = get_the_post_thumbnail( get_the_ID(), 'medium' );
-				echo rhd_picture_frame( $thumb, 'about-thumb' );
-			}
-			?>
-			
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'rhd' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rhd' ), 'after' => '</div>' ) ); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h2 class="page-title"><?php the_title(); ?></h2>
+		<p class="about-quote"><?php echo $quote[0]; ?></p>
+	</header><!-- .entry-header -->
 
-		</div><!-- .entry-content -->
+	<div class="entry-content">
+		<?php
+		if ( has_post_thumbnaiL() ) {
+			$thumb = get_the_post_thumbnail( get_the_ID(), 'medium' );
+			echo rhd_picture_frame( $thumb, 'about-thumb' );
+		}
+		?>
 
-		<footer class="entry-meta">
-			<p><?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post -->
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'rhd' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rhd' ), 'after' => '</div>' ) ); ?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-meta">
+		<p><?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
+	</footer><!-- .entry-meta -->
+</article><!-- #post -->
