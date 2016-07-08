d4c61b138cbdf97aef38337b3c3d8420883a7cb5
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-08 13:37:23 -0400

    dev

--- footer.php
+++ footer.php
@@ -13,10 +13,15 @@
 	</main><!-- #main -->
 
     <footer id="colophon">
+	    <div class="footer-signup">
+		    <?php echo do_shortcode( '[rhd-mailchimp text="Get updates right to your inbox" button="Sign Me Up!"]' ); ?>
+	    </div>
+
         <?php get_sidebar( 'footer' ); ?>
+
         <div class="site-info">
 			<p>
-				<?php echo '&copy;' . date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> <?php echo ( rhd_is_mobile() ) ? '<br>' : '| '; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse<img id="rhd-logo-footer" src="//assets.roundhouse-designs.com/images/rhd-white-house.png" alt="Roundhouse Designs">Designs</a>
+				<?php echo '&copy;' . date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> <?php echo ( rhd_is_mobile() ) ? '<br>' : '| '; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse Designs</a>
             </p>
         </div>
     </footer>
