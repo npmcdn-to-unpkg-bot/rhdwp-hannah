c2b01fcbe3f7fbecf5b37075c8ce9fb2ce4970ed
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-11 12:05:31 -0400

    stuff

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 get_header();
 ?>
 
-	<?php // if ( $_SESSION['blog_area'] === true ) get_template_part( 'template-parts/metabar' ); ?>
-
 	<section id="primary" class="site-content">
 		<div id="content" class="blog-container" role="main">
 
