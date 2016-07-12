d92e78c7fb0d6294ba0cd6552933b5a3c8dc5985
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-12 13:21:00 -0400

    dev

--- inc/rhd-theme.php
+++ inc/rhd-theme.php
@@ -67,3 +67,57 @@ function rhd_custom_excerpt_read_more( $more ) {
 	return rhd_ghost_button( 'Read More', get_permalink( $post ), null, 'center', true, false );
 }
 add_filter( 'excerpt_more', 'rhd_custom_excerpt_read_more' );
+
+
+/**
+ * rhd_subcat_grid function.
+ *
+ * @access public
+ * @param mixed $parent_slug
+ * @param bool $uncat (default: false)
+ * @return void
+ */
+function rhd_subcat_grid( $parent_slug, $uncat = false ) {
+	$args = array(
+		'post_type'			=> 'post',
+		'posts_per_page'	=> 8,
+	);
+
+	$parent = get_category_by_slug( $parent_slug );
+	$parent_id = $parent->term_id;
+	$cats = get_terms( 'category', "child_of=$parent_id" );
+
+	$q = array(); // Array of queries
+	$i = 0;
+	foreach ( $cats as $cat ) {
+		$cat_slug = $cat->slug;
+
+		// Exclude uncategorized (enabled by default)
+		if ( $uncat === false && $cat_slug == 'uncategorized' )
+			continue;
+
+		$args['category_name'] = $cat_slug;
+		$cat = get_category_by_slug( $cat_slug );
+		$cat_id = $cat->term_id;
+		$cat_name = $cat->name;
+		$cat_url = get_category_link( $cat_id );
+
+		$query = new WP_Query( $args );
+		?>
+		<div class="<?php echo $parent_slug; ?>-grid-container <?php echo $cat-slug; ?>-subcat-container subcat-grid-container">
+			<h2 class="subcat-title"><a href="<?php echo $cat_url; ?>" rel="bookmark"><?php echo $cat_name; ?></a></h2>
+			<?php if ( $query->have_posts() ) : ?>
+				<div id="<?php echo $cat_slug; ?>-grid" class="post-grid">
+					<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+						<?php get_template_part( 'template-parts/content', 'grid' ); ?>
+					<?php endwhile; ?>
+				</div>
+				<div class="subcat-more">
+					<?php rhd_ghost_button( 'See More &rarr;', $cat_url, '', 'center', false, true ); ?>
+				</div>
+			<?php endif; ?>
+			<?php unset( $q ); ?>
+		</div>
+		<?php
+	}
+}
---
+++ page-projects.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+<section id="primary" class="site-content full-width">
+	<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<header class="page-header">
+					<h2 class="page-title"><?php the_title(); ?></h2>
+				</header>
+
+				<?php rhd_subcat_grid( 'projects' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+	</div>
+</section>
+
+<?php get_footer(); ?>
