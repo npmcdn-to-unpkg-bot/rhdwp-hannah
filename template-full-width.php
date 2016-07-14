440096277057ab95b636e1370fe4eed4c4301d17
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-14 12:07:05 -0400

    updates

---
+++ template-full-width.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+<section id="primary" class="site-content full-width">
+	<div id="content" role="main">
+		
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_footer(); ?>
