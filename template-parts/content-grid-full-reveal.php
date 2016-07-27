9ce086c23ccbd41c016ba04e884a567a34e31923
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-27 11:17:53 -0400

    notes

---
+++ template-parts/content-grid-full-reveal.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The default template for displaying post grid items (full-ratio thumbnails).
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-grid-item post-grid-item-full' ); ?>>
+	<header class="entry-header">
+		<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
+
+		<?php if ( get_post_type() != 'room-reveal' ) : ?>
+			<p class="entry-details"><?php the_time( get_option( 'date_format' ) ); ?></p>
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-thumbnail">
+			<?php the_post_thumbnail( 'medium' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<div class="entry-summary">
+		<?php the_content(); ?>
+		<?php rhd_ghost_button( 'Read More', get_the_permalink(), '', 'center', true, true ); ?>
+	</div>
+
+	<footer class="entry-meta">
+		<p><?php edit_post_link( __( 'Edit Post', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
+	</footer><!-- .entry-meta -->
+</article><!-- #post -->
---
+++ template-parts/content-single-reveal.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying single posts
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h2 class="entry-title"><?php the_title(); ?></h2>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'rhd' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rhd' ), 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-meta">
+		<p>
+			<?php the_tags( 'Tags: ', ', ' ); ?><br />
+			Filed under: <?php the_category( ' ,' ); ?>
+		</p>
+		<p><?php edit_post_link( __( 'Edit Post', 'rhd' ), '<span class="edit-link">', '</span>' ); ?></p>
+	</footer><!-- .entry-meta -->
+</article><!-- #post -->
+
