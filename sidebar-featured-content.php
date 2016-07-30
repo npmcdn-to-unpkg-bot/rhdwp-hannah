0477d5ca24298b1342f28705d2228904fb97b3dd
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-30 19:07:29 -0400

    slider fix ff

--- sidebar-featured-content.php
+++ sidebar-featured-content.php
@@ -10,12 +10,10 @@
 <?php if ( is_active_sidebar( 'featured-content-area' ) ) : ?>
 	<aside id="featured-content" class="widget-area">
 		<div class="featured-content-wrapper">
-			<?php if( !rhd_is_mobile() ) : ?>
-				<div class="featured-content-slider">
-					<img class="logo-overlay" src="<?php echo RHD_IMG_DIR; ?>/logo-full.png" alt="Vintage Revivals">
-					<?php if ( function_exists( 'soliloquy' ) ) { soliloquy( 'featured-posts', 'slug' ); } ?>
-				</div>
-			<?php endif; ?>
+			<div class="featured-content-slider">
+				<img class="logo-overlay" src="<?php echo RHD_IMG_DIR; ?>/logo-full.png" alt="Vintage Revivals">
+				<?php if ( function_exists( 'soliloquy' ) ) { soliloquy( 'featured-posts', 'slug' ); } ?>
+			</div>
 
 			<div class="featured-links">
 				<?php dynamic_sidebar( 'featured-content-area' ); ?>
