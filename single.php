293e645e4d7a3b950daf690ac63a65d083f61268
Nick Gaswirth <gaswirth@gmail.com>
2016-08-04 19:59:11 -0400

    -Index pages are grids by default -Desktop grid width removed for better customization

---
+++ single.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The single Post template file.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header();
+?>
+
+<section id="primary" class="site-content">
+	<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<?php endwhile; ?>
+
+			<?php if ( comments_open() ) comments_template(); ?>
+
+		<?php endif; // end have_posts() check ?>
+
+	</div><!-- #content -->
+
+	<?php rhd_single_pagination(); ?>
+
+</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
