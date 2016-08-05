762794417d96903e525851ab8906479492876d6b
Nick Gaswirth <gaswirth@gmail.com>
2016-08-05 12:01:50 -0400

    v1.4.11: Better file names

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ define( 'DISALLOW_FILE_EDIT', true );
 include_once( 'inc/rhd-theme.php' );
 include_once( 'inc/rhd-shortcodes.php' );
 include_once( 'inc/rhd-metabar.php' );
-include_once( 'inc/rhd-login-admin.php' );
+include_once( 'inc/rhd-branding.php' );
 
 
 // Globally disable WP toolbar
