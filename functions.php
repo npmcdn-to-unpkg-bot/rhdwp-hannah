bc7b05f369fb5616e2528cb33166b91ca9bfc16f
Nick Gaswirth <nick@roundhouse-designs.com>
2016-08-09 10:54:51 -0400

    rhd-meta-boxes

--- functions.php
+++ functions.php
@@ -28,10 +28,11 @@ define( 'DISALLOW_FILE_EDIT', true );
 
 // Includes
 include_once( 'inc/rhd-shortcodes.php' );
-include_once( 'inc/rhd-theme.php' );
 include_once( 'inc/rhd-metabar.php' );
 include_once( 'inc/rhd-branding.php' );
 include_once( 'inc/rhd-settings.php' );
+include_once( 'inc/rhd-meta-boxes.php' );
+include_once( 'inc/rhd-theme.php' );
 
 
 // Globally disable WP toolbar
