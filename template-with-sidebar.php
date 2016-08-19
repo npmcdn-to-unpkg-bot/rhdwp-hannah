8e70ef6c27b8dfdb3eb61d976b1a9091eaa2693e
Nick Gaswirth <nick@roundhouse-designs.com>
2016-08-19 16:50:39 -0400

    template with sidebar added

---
+++ template-with-sidebar.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Page With Sidebar
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+
+get_header(); ?>
+
+<section id="primary" class="site-content">
+	<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+	</div>
+</section>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
